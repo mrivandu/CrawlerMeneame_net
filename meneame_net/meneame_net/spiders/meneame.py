e2f09b9df852bc8d3576b8973abd80e0ea209d8c
Ruihong Du (Chinasoft International) <v-ruidu@microsoft.com>
2018-12-24 18:03:21 +0800

    Update meneame.py

--- meneame_net/meneame_net/spiders/meneame.py
+++ meneame_net/meneame_net/spiders/meneame.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import requests
 #from meneame_net.items import MeneameNetItem
 
 
@@ -10,37 +11,30 @@ class MeneameSpider(scrapy.Spider):
     start_urls = ['https://www.meneame.net', 'https://www.meneame.net/queue', 'https://www.meneame.net/articles', 'https://www.meneame.net/popular', 'https://www.meneame.net/top_visited']
 
     def parse(self, response):
-        try:
-            total_pages_url = response.xpath("//div[@class='pages margin']/a[last()-1]").extract()[0]
-            print(total_pages_url)
-            total_pages = int(re.findall(r'\d+',total_pages_url)[0])
-            for sub_url in self.start_urls:
-                for page_num in range(1,total_pages+1):
-                    try:
-                        if response.xpath("//div[@class='pages margin']/a[@rel='next']/@href").extract()[0] and response.url == sub_url:
-                            new_sub_url = sub_url + "?page=" + str(page_num)
-                            yield scrapy.Request(new_sub_url,callback=self.sub_parse)
-                    except:
-                        break
-        except:
-            pass
+        for sub_url in self.start_urls:
+            try:
+                sub_url_str = response.xpath("//div[@class='pages margin']/a[last()-1]/@href").extract()[0]
+                total_pages = int(re.findall(r'\d+',sub_url_str)[0])
+                if response.url == sub_url:
+                    for page_num in range(2,total_pages-7667):
+                        new_sub_url = sub_url + "/?page=" +str(page_num)
+                        yield scrapy.Request(new_sub_url,callback=self.sub_parse)
+                else:
+                        continue
+            except:
+                yield scrapy.Request(sub_url,callback=self.sub_parse)
+                continue
 
     def sub_parse(self,response):
-        sub_url_list = response.xpath("//a[@class='comments']/@href").extract()
-        for sub_page_url in sub_url_list:
-            try:
-                new_sub_page_url = 'https://www.meneame.net' + sub_page_url
-                print(new_sub_page_url)
-                yield scrapy.Request(new_sub_page_url,callback=self.target_parse)
-            except:
-                continue
-    
+        target_url_list = response.xpath("//div[@class='news-details-main']/a[@class='comments']/@href").extract()
+        if len(target_url_list) > 0:
+            for target_url in target_url_list:
+                target_page_url = "https://www.meneame.net" + target_url
+                if requests.get(target_page_url).status_code == 200 :
+                    yield scrapy.Request(target_page_url,callback=self.target_parse)
+                else:
+                    continue
+
+
     def target_parse(self,response):
-        pass
-        
-"""
-    def sub_parse(self,response):
-        sub_url_list = response.xpath("//div[@class='news-details-main']/a/@href").extract()
-        for sub_page in sub_url_list:
-            print("https://www.meneame.net"+sub_page)
-"""
+        print('============'+response.url+'============')
